edcdf822564a9ff9ed6cf2427ae316111f5091b3
Aareana Reza <areza5@illinois.edu>
2021-10-27 11:43:54 -0500

    added a Hospital class

---
+++ Hospital.py
@@ -0,0 +1,6 @@
+class Hospital:
+    def __init__(self, name, address, treatments, ranking):
+        self.name = name
+        self.address = address
+        self.treatments = treatments
+        self.ranking = ranking
