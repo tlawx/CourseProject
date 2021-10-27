59f56bb7596d8f7901a0da2893db1ce2f38f1454
Aareana Reza <areza5@illinois.edu>
2021-10-27 15:15:49 -0500

    added Treatment class

---
+++ Treatment.py
@@ -0,0 +1,16 @@
+class Treatment:
+    def __init__(self, treatment_name, treatment_price):
+        self.treatment_name = treatment_name
+        self.treatment_price = treatment_price
+
+    def set_treatment_name(self, name):
+        self.treatment_name = name
+
+    def get_treatment_name(self):
+        return self.treatment_name
+
+    def set_treatment_price(self, price):
+        self.treatment_price = price
+
+    def get_treatment_price(self):
+        return self.treatment_price
