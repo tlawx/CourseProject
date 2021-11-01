5786e4cf2617cca1ecde7f5e04aee9e07df9c460
Aareana Reza <areza5@illinois.edu>
2021-11-01 10:23:41 -0500

    updated Hospital class

--- Hospital.py
+++ Hospital.py
@@ -3,20 +3,18 @@ from Treatment import Treatment
 
 class Hospital:
 
-    def __init__(self, id, name, address=None, link="", treatments=None, ranking=0):
-        self.hospital_id = id
+    def __init__(self, hospital_id, name, city=None, state=None, affiliation=None, link="", treatments=None, rating=0):
+        self.hospital_id = hospital_id
         self.name = name
-        self.address = address
+        self.city = city
+        self.state = state
+        self.affiliation = affiliation
         self.link = link
         self.treatments = [] # tuples of treatment_id, treatment_object?
-        self.ranking = ranking
+        self.rating = rating
 
-        # Might move these later to address class
-        self.city = None
-        self.state = None
-
-    def set_hospital_id(self, id):
-        self.hospital_id = id
+    def set_hospital_id(self, hos_id):
+        self.hospital_id = hos_id
 
     def set_name(self, name):
         self.name = name
@@ -24,11 +22,23 @@ class Hospital:
     def get_name(self):
         return self.name
 
-    def set_address(self, address):
-        self.address = address
+    def set_city(self, city):
+        self.city = city
 
-    def get_address(self):
-        return self.address
+    def get_city(self):
+        return self.city
+
+    def set_state(self, state):
+        self.state = state
+
+    def get_state(self):
+        return self.state
+
+    def set_affiliation(self, affiliation):
+        self.affiliation = affiliation
+
+    def get_affiliation(self):
+        return self.affiliation
 
     def set_link(self, link):
         self.link = link
@@ -39,8 +49,8 @@ class Hospital:
     def add_treatment_object(self, treatment):
         self.treatments.append(treatment)
 
-    def add_new_treatment(self, treatment_name, treatment_price):
-        self.treatments.append(Treatment(treatment_name, treatment_price))
+    def add_new_treatment(self, concept_id, concept_code, vocabulary_id, treatment_name, treatment_price):
+        self.treatments.append(Treatment(concept_id, concept_code, vocabulary_id, treatment_name, treatment_price))
 
     def set_treatments(self, treatments):
         self.treatments = treatments
@@ -48,11 +58,11 @@ class Hospital:
     def get_treatments(self):
         return self.treatments
 
-    def set_ranking(self, ranking):
-        self.ranking = ranking
+    def set_rating(self, rating):
+        self.rating = rating
 
-    def get_ranking(self):
-        return self.ranking
+    def get_rating(self):
+        return self.rating
 
     def __str__(self):
         return f"id: {self.hospital_id}, name: {self.name}"
