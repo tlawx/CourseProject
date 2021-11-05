102a99db829d0cbee6e8eb566599f1c605293b10
Aareana Reza <areza5@illinois.edu>
2021-11-05 10:41:47 -0500

    added hospital variable to Treatment class

--- Treatment.py
+++ Treatment.py
@@ -1,11 +1,15 @@
+from Hospital import Hospital
+
+
 class Treatment:
-    def __init__(self, concept_id, concept_code, vocabulary_id, treatment_name, treatment_price=0.0, category=""):
+    def __init__(self, concept_id, concept_code, vocabulary_id, hospital, treatment_name, treatment_price=0.0, category=""):
         self.concept_id = concept_id
         self.concept_code = concept_code
         self.vocabulary_id = vocabulary_id
         self.treatment_name = treatment_name
         self.treatment_price = treatment_price
         self.category = category
+        self.hospital = hospital
 
     def set_treatment_name(self, name):
         self.treatment_name = name
@@ -24,3 +28,12 @@ class Treatment:
 
     def get_category(self):
         return self.category
+
+    def add_hospital_object(self, hospital):
+        self.hospital = hospital
+
+    def add_new_hospital(self, hospital_id, name, city=None, state=None, affiliation=None, link="", treatments=None, rating=0):
+        self.hospital = Hospital(hospital_id, name, city, state, affiliation, link, treatments, rating)
+
+    def get_hospital(self, hospital):
+        return self.hospital
