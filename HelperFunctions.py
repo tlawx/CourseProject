03779c0d26dd7b8184a1868f4c20cb8f8143fca3
Aareana Reza <areza5@illinois.edu>
2021-11-05 15:30:44 -0500

    changed HelperFunctions to Utilities

--- HelperFunctions.py
+++
@@ -1,9 +0,0 @@
-class HelperFunctions:
-    def __init__(self):
-        pass
-
-    def average_treatment_price(self, treatments):
-        pass
-
-    def treatment_ranking(self):
-        pass
---
+++ Utilities.py
@@ -0,0 +1,19 @@
+class Utilities:
+    def __init__(self):
+        pass
+
+    def average_treatment_price(self, treatments):
+        pass
+
+    def get_relevant_list(self, category, query):
+        # input: category and query
+        # output: a list of relevant treatments with their associated hospitals,
+        #         ranked by price and rating (call rank_list)
+        #         each treatment listed will have name, price, hospital name, hospital city,
+        #         hospital state, hospital link
+        pass
+
+    def rank_list(self, relevant_treatments):
+        # input: relevant treatments based on the category and query
+        # output: list ranked by price and rating
+        pass
