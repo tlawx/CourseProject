d925c1679fc7e6dbec74e8277ff8b689fb07414d
Morgan Whitman <whitman5@illinois.edu>
2021-11-15 17:07:15 -0600

    added category files

--- Search.py
+++ Search.py
@@ -1,42 +1,29 @@
+from meta import Meta
 
 
 class Search:
     treatment_codes = []  # one dim
     treatment_long_description_list = []  # two (2) dims ?
     categories = []
-    category_name = ''
-    query = ''
-    category_code = ''
 
-    def __init__(self):
-        pass
-
-    def add_treatments(self, treatment_descriptions_long, treatment_codes, categories):
-        self.treatment_codes = treatment_codes # one dim
-        self.treatment_long_description_list = treatment_descriptions_long # two (2) dims ?
-        self.categories = categories
-
-    def new_query(self, category_code, category_name, query):
+    def __init__(self, category_code, query):
         self.category_code = category_code
-        self.category_name = category_name
         self.query = query
 
     def find_possible_treatments(self):
-        # search algorithm to return list of treatments within category that match string: 1)determine if hcpcs code 2) parse phrase & find matches within code
-
         # OUTLINE:
-        # 1. get list of treatments that start with category_code
-        category_treatments = get_treatments_in_category(self.category_code)
+        # 1. get correct config file from a method in driver
 
         # 2. search for relevant treatments from the category
-        relevant_treatments = get_relevant_treatments(category_treatments, self.query)
 
-        # 3. rank relevant treatments
-        ranked_treatments = get_ranked_treatments(relevant_treatments)
+        m = Meta()
+        relevant_treatments = m.search_inverted_index(self.query)
+        # print(relevant_treatments)
 
-        # 4. print out treatments, their hospitals, price, hospital link, etc, in a nice format
-        print_treatment_info(ranked_treatments)
+        # 3. print out treatments, their hospitals, price, hospital link, etc, in a nice format
+        # print_treatment_info(relevant_treatments)
 
+    def find_treatments(self):
         if self.query in self.treatment_codes: # HOLD : assume passed HCPCS code
             print("Matched a treatment!")
             return self.query
@@ -60,6 +47,10 @@ def get_ranked_treatments(relevant_treatments):
     ranked_treatments = []
     return ranked_treatments
 
+def add_treatments(self, treatment_descriptions_long, treatment_codes, categories):
+    self.treatment_codes = treatment_codes # one dim
+    self.treatment_long_description_list = treatment_descriptions_long # two (2) dims ?
+    self.categories = categories
 
 def print_treatment_info(treatments):
     # TODO: print (for each treatment) treatment name, treatment price, hospital name, hospital location, hospital link
@@ -68,16 +59,19 @@ def print_treatment_info(treatments):
 
 if __name__ == "__main__":
 
-    test_treatment_codes = ['A0021', 'B4034', 'C1300'] # load list of treatment HCPCS
-    test_treatment_descriptions_long = ['Ambulance service, outside state per mile, transport (medicaid only)', 'Enteral feeding supply kit; syringe fed, per day, includes but not limited to feeding/flushing syringe, administration set tubing, dressings, tape','Hyperbaric oxygen under pressure, full body chamber, per 30 minute interval']
-    test_treatment_descriptions_short = ['Outside state ambulance serv', 'Enter feed supkit syr by day','Hyperbaric oxygen']
-    test_categories = [] # pull in query
-    test_query = 'A0020' # pull in query
-    test_query_category = '' # pull in query
+    s = Search("A", "Replacement")
+    s.find_possible_treatments()
 
-    search = Search()
-    search.add_treatments(test_treatment_descriptions_long, test_treatment_codes, test_categories)
-    search.new_query("A", test_query_category, test_query)
+    # test_treatment_codes = ['A0021', 'B4034', 'C1300'] # load list of treatment HCPCS
+    # test_treatment_descriptions_long = ['Ambulance service, outside state per mile, transport (medicaid only)', 'Enteral feeding supply kit; syringe fed, per day, includes but not limited to feeding/flushing syringe, administration set tubing, dressings, tape','Hyperbaric oxygen under pressure, full body chamber, per 30 minute interval']
+    # test_treatment_descriptions_short = ['Outside state ambulance serv', 'Enter feed supkit syr by day','Hyperbaric oxygen']
+    # test_categories = [] # pull in query
+    # test_query = 'A0020' # pull in query
+    # test_query_category = '' # pull in query
+    #
+    # search = Search()
+    # search.add_treatments(test_treatment_descriptions_long, test_treatment_codes, test_categories)
+    # search.new_query("A", test_query_category, test_query)
     
     #results = search.find_possible_treatments(test_query_category, test_query) # treatment list
 
