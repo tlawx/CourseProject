3275a11c8e3e6301ebf0447dcac2226571fecd55
Aareana Reza <areza5@illinois.edu>
2021-11-18 14:50:04 -0600

    added hospital dict to Treatment class

--- Treatment.py
+++ Treatment.py
@@ -2,14 +2,14 @@ from Hospital import Hospital
 
 
 class Treatment:
-    def __init__(self, concept_id, concept_code, vocabulary_id, treatment_name, hospital="", treatment_price=0.0, category=""):
+    def __init__(self, concept_id, concept_code, vocabulary_id, treatment_name, hospitals={}, treatment_price=0.0, category=""):
         self.concept_id = concept_id
         self.concept_code = concept_code
         self.vocabulary_id = vocabulary_id
         self.treatment_name = treatment_name
         self.treatment_price = treatment_price
         self.category = category
-        self.hospital = hospital
+        self.hospitals = hospitals
 
     def set_treatment_name(self, name):
         self.treatment_name = name
@@ -29,11 +29,12 @@ class Treatment:
     def get_category(self):
         return self.category
 
-    def add_hospital_object(self, hospital):
-        self.hospital = hospital
+    def add_hospital_dict(self, hospital_id, hospital_object):
+        self.hospitals[hospital_id] = hospital_object
 
     def add_new_hospital(self, hospital_id, name, city=None, state=None, affiliation=None, link="", treatments=None, rating=0):
-        self.hospital = Hospital(hospital_id, name, city, state, affiliation, link, treatments, rating)
+        hospital = Hospital(hospital_id, name, city, state, affiliation, link, treatments, rating)
+        self.hospitals[hospital_id] = hospital
 
     def get_hospital(self):
-        return self.hospital
+        return self.hospitals
