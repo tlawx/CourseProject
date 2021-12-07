962549b099469c2d89a9c3e7aee8a40a215880a9
Aareana Reza <areza5@illinois.edu>
2021-12-07 17:17:08 -0600

    removed unused code from Search

--- Search.py
+++ Search.py
@@ -28,23 +28,11 @@ class Search:
         
         return rel_treatment_objects
 
-    def find_treatments(self):
-        if self.query in self.treatment_codes: # HOLD : assume passed HCPCS code
-            print("Matched a treatment!")
-            return self.query
-        print("Did not find treatment. Please enter search with another description or HCPCS code.")
-
-    def add_treatments(self, treatment_descriptions_long, treatment_codes, categories):
-        self.treatment_codes = treatment_codes # one dim
-        self.treatment_long_description_list = treatment_descriptions_long # two (2) dims ?
-        self.categories = categories
-
     def print_treatment_info(treatments):
         # TODO: print (for each treatment) treatment name, treatment price, hospital name, hospital location, hospital link
         pass
     
     def get_treatment_list(self, treatments):
-        # TODO: 
         index_list = []
         for tup in treatments: 
             index_list.append(tup[0])
@@ -103,8 +91,6 @@ class Search:
 
 if __name__ == "__main__":
 
-    
-
     driver = Driver()
 
     driver.categories_dataset = 'data/hcpcs_categories.csv'
@@ -132,31 +118,5 @@ if __name__ == "__main__":
     
     s = Search(test_category, test_query)
     rel_treatment_tuples = s.find_possible_treatments()
-    
-    #rel_treatment_list = s.get_treatment_list(rel_treatment_tuples)
-    #rel_treatment_objects = s.get_treatment_objects(rel_treatment_list)
-
-
-    #if rel_treatment_list is []: 
-     #   print("Best Matches: ")
-      #  for i in rel_treatment_list: 
-       #     print(category_dict_list_hcpcs['A'][i]+" "+category_treatment_dict['A'][category_dict_list_hcpcs['A'][i]].treatment_name)
-    #else: 
-     #   print("No Found Matches")
-
-
-    #for testing
-    # test_treatment_codes = ['A0021', 'B4034', 'C1300'] # load list of treatment HCPCS
-    # test_treatment_descriptions_long = ['Ambulance service, outside state per mile, transport (medicaid only)', 'Enteral feeding supply kit; syringe fed, per day, includes but not limited to feeding/flushing syringe, administration set tubing, dressings, tape','Hyperbaric oxygen under pressure, full body chamber, per 30 minute interval']
-    # test_treatment_descriptions_short = ['Outside state ambulance serv', 'Enter feed supkit syr by day','Hyperbaric oxygen']
-    # test_categories = [] # pull in query
-    # test_query = 'A0020' # pull in query
-    # test_query_category = '' # pull in query
-    #
-    # search = Search()
-    # search.add_treatments(test_treatment_descriptions_long, test_treatment_codes, test_categories)
-    # search.new_query("A", test_query_category, test_query)
-    
-    #results = search.find_possible_treatments(test_query_category, test_query) # treatment list
 
         
