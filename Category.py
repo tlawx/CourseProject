4375b954b6ad4de954f65060f4cd4d63dd9eddc3
Aareana Reza <areza5@illinois.edu>
2021-12-08 11:45:09 -0600

    removed unused code from Category

--- Category.py
+++ Category.py
@@ -7,23 +7,3 @@ class Category:
         self.name = name
         self.treatments = treatments
 
-    def set_name(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
-
-    def set_letter(self, letter):
-        self.letter = letter
-
-    def get_letter(self):
-        return self.letter   
-
-    def add_treatment_object(self, treatment):
-        self.treatments.append(treatment)
-
-    def add_new_treatment(self, treatment_name, treatment_price):
-        self.treatments.append(Treatment(treatment_name, treatment_price))
-
-    def set_treatments(self, treatments):
-        self.treatments = treatments
