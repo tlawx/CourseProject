d0b91955ac601ba4e3973f55eb1b1eeac1f42360
Aareana Reza <areza5@illinois.edu>
2021-12-08 11:45:36 -0600

    removed unused code from Treatment

--- Treatment.py
+++ Treatment.py
@@ -1,5 +1,3 @@
-from Hospital import Hospital
-
 
 class Treatment:
     def __init__(self, concept_id, concept_code, vocabulary_id, treatment_name, hospitals={}, treatment_price=0.0, category=""):
@@ -11,30 +9,12 @@ class Treatment:
         self.category = category
         self.hospitals = hospitals
 
-    def set_treatment_name(self, name):
-        self.treatment_name = name
-
     def get_treatment_name(self):
         return self.treatment_name
-
-    def set_treatment_price(self, price):
-        self.treatment_price = price
 
     def get_treatment_price(self):
         return self.treatment_price
 
-    def set_category(self, category):
-        self.category = category
-
-    def get_category(self):
-        return self.category
-
     def add_hospital_dict(self, hospital_id, hospital_object):
         self.hospitals[hospital_id] = hospital_object
 
-    def add_new_hospital(self, hospital_id, name, city=None, state=None, affiliation=None, link="", treatments=None, rating=0):
-        hospital = Hospital(hospital_id, name, city, state, affiliation, link, treatments, rating)
-        self.hospitals[hospital_id] = hospital
-
-    def get_hospital(self):
-        return self.hospitals
