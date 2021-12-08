b3ae51eb0a1e59e76899953c85c7e3688fa6389e
Aareana Reza <areza5@illinois.edu>
2021-12-08 11:45:27 -0600

    removed unused code from Patient

--- Patient.py
+++ Patient.py
@@ -1,41 +1,6 @@
-from Treatment import Treatment
-
 
 class Patient:
     def __init__(self, name, city):
         self.name = name
         self.city = city
 
-    treatments_needed = []
-    queries = []
-    selected_hospitals = []
-
-    def set_name(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
-
-    def set_city(self, city):
-        self.city = city
-
-    def get_city(self):
-        return self.city
-
-    def add_query(self, new_query):
-        self.queries.append(new_query)
-
-    def add_treatment_object(self, treatment):
-        self.treatments_needed.append(treatment)
-
-    def add_new_treatment(self, treatment_name, treatment_price):
-        self.treatments_needed.append(Treatment(treatment_name, treatment_price))
-
-    def set_treatments(self, treatments):
-        self.treatments_needed = treatments
-
-    def get_treatments(self):
-        return self.treatments_needed
-
-    def add_hospital(self, hospital):
-        self.selected_hospitals.append(hospital)
