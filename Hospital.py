d84d5377784acc1ede5eba76ab2fa4d8a14bcf7c
Aareana Reza <areza5@illinois.edu>
2021-12-08 11:45:18 -0600

    removed unused code from Hospital

--- Hospital.py
+++ Hospital.py
@@ -1,5 +1,3 @@
-#from Treatment import Treatment
-
 
 class Hospital:
 
@@ -11,59 +9,7 @@ class Hospital:
         self.state = state
         self.affiliation = affiliation
         self.link = link
-        self.treatments = [] # tuples of treatment_id, treatment_object?
-        self.rating = rating
-
-    def set_hospital_id(self, hos_id):
-        self.hospital_id = hos_id
-
-    def set_name(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
-
-    def set_city(self, city):
-        self.city = city
-
-    def get_city(self):
-        return self.city
-
-    def set_state(self, state):
-        self.state = state
-
-    def get_state(self):
-        return self.state
-
-    def set_affiliation(self, affiliation):
-        self.affiliation = affiliation
-
-    def get_affiliation(self):
-        return self.affiliation
-
-    def set_link(self, link):
-        self.link = link
-
-    def get_link(self):
-        return self.link
-
-    def add_treatment_object(self, treatment):
-        self.treatments.append(treatment)
-
-    # def add_new_treatment(self, concept_id, concept_code, vocabulary_id, treatment_name, treatment_price):
-    #     self.treatments.append(Treatment(concept_id, concept_code, vocabulary_id, treatment_name, treatment_price))
-
-    def set_treatments(self, treatments):
-        self.treatments = treatments
-
-    def get_treatments(self):
-        return self.treatments
-
-    def set_rating(self, rating):
-        self.rating = rating
-
-    def get_rating(self):
-        return self.rating
+        self.treatments = []
 
     def __str__(self):
         return "Hospital NPI: {}, Hospital Name: {}".format(self.npi, self.name)
